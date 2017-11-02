3b6e490526a8f21da30d98dde258e3af300b4650
4419-4-11\user <saiken0917@gmail.com>
2017-11-02 21:15:47 +0800

    1.完成犀牛生命邏輯

--- Assets/Scripts/RhinoController.cs
+++ Assets/Scripts/RhinoController.cs
@@ -26,6 +26,7 @@ public class RhinoController : MonoBehaviour
     private AudioSource audioSource;
     private Animator animator;
 
+    private bool isImpacting;
     public float hitOffset = 1;
     public Transform target;
 
@@ -37,11 +38,43 @@ public class RhinoController : MonoBehaviour
     }
     private void OnEnable()
     {
-        StartCoroutine("ProcessImpact");
+        GameObject go = GameObject.FindGameObjectWithTag(crystalTag);
+        if (go != null)
+        {
+            target = go.transform;
+            StartCoroutine("ProcessState");
+        }
+    }
+    private IEnumerator ProcessState()
+    {
+        while (target != null)
+        {
+            //隨機產生以水晶為圓心，半徑為衝撞距離的一點，讓犀牛前往
+            navMeshAgent.speed = walkingSpeed;
+            float randomRad = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+            float distance = Vector3.Distance(impactTarget.position, transform.position);
+            Vector3 randomPos = target.position + new Vector3(Mathf.Cos(randomRad), 0, Mathf.Sin(randomRad)) * distance;
+            navMeshAgent.SetDestination(randomPos);
+            yield return null;
+            //等待犀牛走到目的地
+            while (navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance)
+            {
+                yield return null;
+            }
+            if (target == null)
+            {
+                yield break;
+            }
+            //開始攻擊
+            yield return StartCoroutine("ProcessImpact");
+            //攻擊結束
+            yield return new WaitForSeconds(2f);
+        }
     }
 
     private IEnumerator ProcessImpact()
     {
+        transform.LookAt(target);//犀牛面相target
         aimSlider.gameObject.SetActive(true);
         aimSlider.value = aimSlider.minValue;
         aimSlider.maxValue = impactChargeTime;
@@ -51,6 +84,7 @@ public class RhinoController : MonoBehaviour
             aimSlider.value += Time.deltaTime;
             yield return null;
         }
+        aimSlider.gameObject.SetActive(false);
         navMeshAgent.speed = impactSpeed;
         RaycastHit hit;
         float distance = Vector3.Distance(impactTarget.position, transform.position);
@@ -63,6 +97,14 @@ public class RhinoController : MonoBehaviour
             }
         }
         navMeshAgent.SetDestination(targetPos - transform.forward * hitOffset);
+        yield return null;
+        //等待犀牛衝到目的地
+        isImpacting = true;
+        while (navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance)
+        {
+            yield return null;
+        }
+        isImpacting = false;
     }
     private void Update()
     {
